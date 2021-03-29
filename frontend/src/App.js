ae17e9954ffc1bc36b47014d27ab6ffde6792848
Dmytro <dmytro@MacBook-Pro.local>
2021-03-29 08:12:28 +0200

    front_end

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React from 'react';
+import { Button } from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+function App() {
+  return (
+    
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="xl">
+
+      </Container>
+    </React.Fragment>
+  );
+}
+
+export default App;
